e5dc1b9d26e96b9db90a652afa0c2b912e9468d3
DogukanBaloglu <dogukanbaloglu@hotmail.com>
2020-08-24 15:03:50 +0300

    v1

--- RickandMorty/__init__.py
+++ RickandMorty/__init__.py
@@ -1,7 +1,7 @@
 from RickandMorty.base import Base
-from RickandMorty.character_client import *
-from RickandMorty.episode_client import *
-from RickandMorty.location_client import *
+from RickandMorty.character_client import Characters
+from RickandMorty.episode_client import Episodes
+from RickandMorty.location_client import Locations
 
 
 
