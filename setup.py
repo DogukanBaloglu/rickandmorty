1d27effa5cc2f19e74b028aa65775f1c065ca182
DogukanBaloglu <dogukanbaloglu@hotmail.com>
2020-08-25 18:58:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,13 @@ setup(
     name='rickandmorty',
     author='Dogukan Baloglu',
     author_email='dogukanbaloglu99@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
     packages=find_packages(include=['rickandmorty'],exclude=['tests*']),
     version='0.1.0',
     description='Rick and Morty client for Rick and Morty API (https://rickandmortyapi.com/) ',
